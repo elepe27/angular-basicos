4c34fd3a02413d5f5caa837c57b7f01c02622a8f
Esteban Lepe <esteban.lepe1c@gmail.com>
2021-03-21 16:32:28 -0300

    Mostrar Listas

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder:number;
+}
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
+  
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent  {
 
-  constructor() { }
+  personajes : Personaje [] = [
+    {
+      nombre: 'Goku',
+      poder:15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder:7500
+    },
+    
+  ] ;
 
-  ngOnInit(): void {
+  nuevo :Personaje={
+    nombre:'',
+    poder:0
   }
+  agregar(){
+    if(this.nuevo.nombre.trim().length===0){return;}
 
+    console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);
+
+    this.nuevo = {
+      nombre:'',
+      poder:0
+    }
+  }
+  
+  
 }
